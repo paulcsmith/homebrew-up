72f1ecbc7973bc98af8be27db9ab3a113cf3229b
Paul Smith <paulcsmith0218@gmail.com>
2019-04-04 22:53:34 -0400

    Bump version to 0.1.6

--- Formula/docker-up.rb
+++ Formula/docker-up.rb
@@ -1,15 +1,12 @@
 class DockerUp < Formula
   desc "Tool for local developement with Docker"
   homepage "http://github.com/paulcsmith/up"
-  url "https://github.com/paulcsmith/homebrew-up/raw/master/tarballs/docker-up-0.1.5.tar.gz"
-  sha256 "36c710c82246f899bc19ce643232325d5d46c77f69aab9fe94db1077e6a6e168"
-  version "0.1.5"
+  url "https://github.com/paulcsmith/homebrew-up/raw/master/tarballs/docker-up-0.1.6.tar.gz"
+  sha256 "79978199e12a85430fd7351e05e7691c9a5df27ce312a6ca8c2f21d58f4faaff"
+  version "0.1.6"
   depends_on "crystal-lang"
 
   def install
-    system "ls"
-    system "echo ls"
-    system "echo $PWD"
     system "crystal build lib/up/src/run.cr -o up"
     bin.install "up"
   end
