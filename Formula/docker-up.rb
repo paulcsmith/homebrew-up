1dae64ee2475962f8558b84dad3f5f929127ea1a
Paul Smith <paulcsmith0218@gmail.com>
2019-04-24 09:12:47 -0400

    Release 0.1.7

--- Formula/docker-up.rb
+++ Formula/docker-up.rb
@@ -1,9 +1,9 @@
 class DockerUp < Formula
   desc "Tool for local developement with Docker"
   homepage "http://github.com/paulcsmith/up"
-  url "https://github.com/paulcsmith/homebrew-up/raw/master/tarballs/docker-up-0.1.6.tar.gz"
-  sha256 "79978199e12a85430fd7351e05e7691c9a5df27ce312a6ca8c2f21d58f4faaff"
-  version "0.1.6"
+  url "https://github.com/paulcsmith/homebrew-up/raw/master/tarballs/docker-up-0.1.7.tar.gz"
+  sha256 "ccd66312180e5999d368cb4b24e193695e76fd1c0f70087371fbc408fca41f9c"
+  version "0.1.7"
   depends_on "crystal-lang"
 
   def install
